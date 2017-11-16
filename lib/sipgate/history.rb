45ac6fdd53c4e4db4972945ff10f9e75f41e407f
Gilles Crofils <gilles@secondbureau.com>
2017-11-16 19:19:09 +0100

    added history

---
+++ lib/sipgate/history.rb
@@ -0,0 +1,50 @@
+module Sipgate
+  class History
+    
+    ATTRIBUTES = [:id, :source, :target, :sourceAlias, :targetAlias, :type, :created, :lastModified, :direction, :status, :documentUrl, :reportUrl, :previewUrl].freeze
+    
+    attr_accessor *ATTRIBUTES
+    
+    def initialize(params)
+      ATTRIBUTES.each do |att|
+        send("#{att}=", params[att.to_s])
+      end
+    end
+    
+    def self.default_userid
+      Sipgate.user_id || Sipgate::User.first_user_id
+    end
+    
+    def self.all
+      @histories ||= get_history
+    end
+    
+    def self.find_by_id(entryid)
+      get_history(entryid)
+    end
+    
+    end
+    
+    private
+    
+    def self.get_history(userid = nil, entryid=nil)
+      userid ||= default_userid
+      response = Sipgate::Connexion.conn.get do |req|
+        req.url "/v1/#{userid}/history/#{entryid}"
+      end
+      if response.status.eql?(200)
+        body = JSON.parse(response.body)
+        if body.has_key?('items')
+          [].tap do |histories|
+            ['items'].each do |item|
+              histories << Sipgate::History.new(item)
+            end
+          end
+        else
+          Sipgate::History.new(body)
+        end
+      end
+    end
+    
+  end
+end
