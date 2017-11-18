e48ca19b5738d1f18214ee12246da2620637d60e
Gilles Crofils <gilles@secondbureau.com>
2017-11-18 20:02:07 +0100

    added error handler

--- lib/sipgate/fax.rb
+++ lib/sipgate/fax.rb
@@ -2,6 +2,7 @@ module Sipgate
   class Fax
     
     class SipgateError < StandardError ; end
+    class RequestError < StandardError ; end
     
     attr_accessor :fax_number, :pdf, :faxline_id, :filename
     
@@ -34,6 +35,7 @@ module Sipgate
                   base64Content: Base64.strict_encode64(pdf)}.to_json
       end
       raise SipgateError, 'Sipgate returns HTTP 500' if response.status.eql?(500)
+      raise RequestError, 'Sipgate returns HTTP 400' if response.status.eql?(400)
       raise Exception     unless response.status.eql?(200)
       JSON.parse(response.body)['sessionId']
     end
